e00eaf8f46bb85ffd41ecd88ca11089c2dc04608
Kunal Diwan <86975698+Kunal-Diwan@users.noreply.github.com>
2021-10-29 20:16:04 +0530

    Trying to fix 😅🙂

--- plugins/help.py
+++ plugins/help.py
@@ -7,7 +7,7 @@ logging.basicConfig(level=logging.INFO)
 
 @Client.on_message(filters.private & filters.incoming & filters.command(['help']))
 def _help(client, message):
-    await client.send_message(chat_id = message.chat.id,
+    client.send_message(chat_id = message.chat.id,
         text = tr.HELP_MSG[1],
         parse_mode="markdown",
         disable_notification = True,
