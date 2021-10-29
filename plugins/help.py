2a65e219d9975cabe992aa4b7945eed6c3c31eeb
Kunal Diwan <86975698+Kunal-Diwan@users.noreply.github.com>
2021-10-29 20:24:11 +0530

    Update help.py

--- plugins/help.py
+++ plugins/help.py
@@ -6,7 +6,7 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 logging.basicConfig(level=logging.INFO)
 
 @Client.on_message(filters.private & filters.incoming & filters.command(['help']))
-def _help(client, message):
+async def _help(client, message):
     client.send_message(chat_id = message.chat.id,
         text = tr.HELP_MSG[1],
         parse_mode="markdown",
@@ -18,7 +18,7 @@ def _help(client, message):
 help_callback_filter = filters.create(lambda _, __, query: query.data.startswith('help+'))
 
 @Client.on_callback_query(help_callback_filter)
-def help_answer(client, callback_query):
+async def help_answer(client, callback_query):
     chat_id = callback_query.from_user.id
     message_id = callback_query.message.message_id
     msg = int(callback_query.data.split('+')[1])
