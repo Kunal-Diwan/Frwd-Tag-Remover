cdd4eab575340a4d97b41283c83ae54f3f98874d
Kunal Diwan <86975698+Kunal-Diwan@users.noreply.github.com>
2021-10-29 18:31:19 +0530

    Added Class messages

--- config.py
+++ config.py
@@ -58,3 +58,16 @@ logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
 def LOGGER(name: str) -> logging.Logger:
     return logging.getLogger(name)
+
+class Messages():
+      HELP_MSG = [
+        ".",
+
+        "**Features 🧰**\n\n◆ <b>Works Only in channel</b> \n\n◆ Fastest Automatic Forward Remover \n\n◆ Forward a message/file/Text/media to the channel \n\n◆ Automatically Deletes The Forward Tag and Resends the Message",
+
+        "**Setup**\n\n<b>👉 First of all add me to your channel as admin with all permission .\n\n<b><u>⚠️ Note ⚠️</u> - Only Channel Creator can set me and i will leave the channel if i am not an admin in the channel. </b>",
+
+        "**Excellent**\n\n<u>All Setup Done</u> \n\n<i>Now, I will remove any type of forwarded post from that channel will and send it again without forward tag .</i>",
+
+        "**Report a Problem**\n\nIf something <b>unexpected</b> happens, you can report it to us. (You can also suggest features.)\n\n<b>Steps</b>\n1) Try whatever you did again. If it shows the same unexpected thing, move to step 2 \n2) Visit @DevelopedBotz and define your problem <b>completely</b>, i.e, what you expected and what happened instead.If you don't get a reply, tag an admin."
+      ]
