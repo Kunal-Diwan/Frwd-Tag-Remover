ae6f46b21ca72ce3cabc0ca04da725f828b0c43e
Kunal Diwan <86975698+Kunal-Diwan@users.noreply.github.com>
2021-10-30 20:59:08 +0530

    Update help.py

--- plugins/help.py
+++ plugins/help.py
@@ -19,10 +19,14 @@ help_callback_filter = filters.create(lambda _, __, query: query.data.startswith
 
 @Client.on_callback_query(help_callback_filter)
 async def help_answer(client, callback_query):
-    msg = int(callback_query.data.split('+')[1])
-    Client.edit_message_text(chat_id = callback_query.from_user.id, message_id = callback_query.message.message_id,
-    text=tr.HELP_MSG[msg],    reply_markup=InlineKeyboardMarkup(map(msg))
-    )
+    chat_id = callback_query.from_user.id,
+    message_id = callback_query.message.message_id,
+    msg = int(callback_query.data.split('+')[1]),
+    client.edit_message_text(
+        chat_id, message_id, message.text,
+        text=tr.HELP_MSG[msg],
+        reply_markup=InlineKeyboardMarkup(map(msg))
+        disable_web_page_preview=True)
 
 
 def map(pos):
