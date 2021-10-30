48c6df7dfa23eec31754569e123d6d0a48284760
Kunal Diwan <86975698+Kunal-Diwan@users.noreply.github.com>
2021-10-30 15:11:04 +0530

    Update help.py

--- plugins/help.py
+++ plugins/help.py
@@ -23,7 +23,7 @@ async def help_answer(client, callback_query):
     message_id = callback_query.message.message_id
     msg = int(callback_query.data.split('+')[1])
     Client.edit_message_text(
-    chat_id, message_id, text=tr.HELP_MSG[msg],,
+    chat_id, message_id, text=tr.HELP_MSG[msg],
     disable_web_page_preview=True)
 
 
