81610001228e6bd08d2a181a250143aeecfe2b0a
Kunal Diwan <86975698+Kunal-Diwan@users.noreply.github.com>
2021-10-30 21:31:45 +0530

    Update config.py

--- config.py
+++ config.py
@@ -63,11 +63,11 @@ class Messages():
       HELP_MSG = [
         ".",
 
-        "**Force Subscribe**\n```𝐅𝐨𝐫𝐜𝐞 𝐠𝐫𝐨𝐮𝐩 𝐦𝐞𝐦𝐛𝐞𝐫𝐬 𝐭𝐨 𝐣𝐨𝐢𝐧 𝐚 𝐬𝐩𝐞𝐜𝐢𝐟𝐢𝐜 𝐜𝐡𝐚𝐧𝐧𝐞𝐥 𝐛𝐞𝐟𝐨𝐫𝐞 𝐬𝐞𝐧𝐝𝐢𝐧𝐠 𝐦𝐞𝐬𝐬𝐚𝐠𝐞𝐬 𝐢𝐧 𝐭𝐡𝐞 𝐠𝐫𝐨𝐮𝐩.\n𝐈 𝐰𝐢𝐥𝐥 𝐦𝐮𝐭𝐞 𝐦𝐞𝐦𝐛𝐞𝐫𝐬 𝐢𝐟 𝐭𝐡𝐞𝐲 𝐧𝐨𝐭 𝐣𝐨𝐢𝐧𝐞𝐝 𝐲𝐨𝐮𝐫 𝐜𝐡𝐚𝐧𝐧𝐞𝐥 𝐚𝐧𝐝 𝐭𝐞𝐥𝐥 𝐭𝐡𝐞𝐦 𝐭𝐨 𝐣𝐨𝐢𝐧 𝐭𝐡𝐞 𝐜𝐡𝐚𝐧𝐧𝐞𝐥 𝐚𝐧𝐝 𝐮𝐧𝐦𝐮𝐭𝐞 𝐭𝐡𝐞𝐦𝐬𝐞𝐥𝐟 𝐛𝐲 𝐩𝐫𝐞𝐬𝐬𝐢𝐧𝐠 𝐚 𝐛𝐮𝐭𝐭𝐨𝐧.```",
-        
-        "**Setup**\n```𝐅𝐢𝐫𝐬𝐭 𝐨𝐟 𝐚𝐥𝐥 𝐚𝐝𝐝 𝐦𝐞 𝐢𝐧 𝐭𝐡𝐞 𝐠𝐫𝐨𝐮𝐩 𝐚𝐬 𝐚𝐝𝐦𝐢𝐧 𝐰𝐢𝐭𝐡 𝐛𝐚𝐧 𝐮𝐬𝐞𝐫𝐬 𝐩𝐞𝐫𝐦𝐢𝐬𝐬𝐢𝐨𝐧 𝐚𝐧𝐝 𝐢𝐧 𝐭𝐡𝐞 𝐜𝐡𝐚𝐧𝐧𝐞𝐥 𝐚𝐬 𝐚𝐝𝐦𝐢𝐧.\n𝐍𝐨𝐭𝐞 ⚠️ : 𝐎𝐧𝐥𝐲 𝐜𝐫𝐞𝐚𝐭𝐨𝐫 𝐨𝐟 𝐭𝐡𝐞 𝐠𝐫𝐨𝐮𝐩 𝐜𝐚𝐧 𝐬𝐞𝐭𝐮𝐩 𝐦𝐞 𝐚𝐧𝐝 𝐢 𝐰𝐢𝐥𝐥 𝐥𝐞𝐚𝐯𝐞 𝐭𝐡𝐞 𝐜𝐡𝐚𝐭 𝐢𝐟 𝐢 𝐚𝐦 𝐧𝐨𝐭 𝐚𝐧 𝐚𝐝𝐦𝐢𝐧 𝐢𝐧 𝐭𝐡𝐞 𝐜𝐡𝐚𝐭.```",
-        
-        "**Commmands**\n```/ForceSubscribe``` - To get the current settings.\n```/ForceSubscribe no/off/disable``` - To turn of ForceSubscribe.\n```/ForceSubscribe {channel username}``` - To turn on and setup the channel.\n```/ForceSubscribe clear``` - To unmute all members who muted by me.\n\nNote: **/FSub is an alias of /ForceSubscribe**",
-        
+        "**Features 🧰**\n\n◆ <b>Works Only in channel</b> \n\n◆ Fastest Automatic Forward Remover \n\n◆ Forward a message/file/Text/media to the channel \n\n◆ Automatically Deletes The Forward Tag and Resends the Message",
+
+        "**Setup**\n\n<b>👉 First of all add me to your channel as admin with all permission .\n\n<b><u>⚠️ Note ⚠️</u> - Only Channel Creator can set me and i will leave the channel if i am not an admin in the channel. </b>",
+
+        "**Excellent**\n\n<u>All Setup Done</u> \n\n<i>Now, I will remove any type of forwarded post from that channel will and send it again without forward tag .</i>",
+
         "**𝐑𝐞𝐩𝐨𝐫𝐭 𝐁𝐮𝐠𝐬 🔽**"
       ]
